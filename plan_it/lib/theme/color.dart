37b1a1f475a0863ee04dfd205256822928961465
Rayhan Pervej <103215390+Rayhan-Pervej@users.noreply.github.com>
2024-03-24 02:27:11 +0600

    fix issue

---
+++ plan_it/lib/theme/color.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyColor{
+
+  // BackGround Color
+  static const appbarColor = Color(0xffffffff);
+  static const scaffoldColor= Color(0xfff1f2f6);
+
+  // Button Color
+  static const buttonBlue = Color(0xff2e3192);
+  static const buttonWhite = Color(0xffffffff);
+
+  // Container
+  static const containerWhite = Colors.white;
+  static const containerBlue = Color(0xff5271ff);
+  static const containerRed = Color(0xffff5252);
+  static const containerGray = Color(0xff5c6166);
+
+  // Alert
+  static const alertBlue = Color(0xff256ed1);
+  static const alertRed =  Color(0xffff5252);
+
+  //Text field
+  static const fieldGray = Color(0xfff1f2f6);
+
+  // Header Text
+  static const headerBlue = Color(0xff3232d2);
+  static const headerBlack = Colors.black;
+
+  // Normal Text/line
+  static const black = Colors.black;
+  static const graySoft = Color(0xff5c6166);
+  static const gray = Color(0xff38383a);
+  static const green = Color(0xff00bf63);
+  static const white = Colors.white;
+  static const blue = Color(0xff256ed1);
+}
---
+++ plan_it/lib/theme/text.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'color.dart';
+
+class TextDesign {
+
+  TextStyle pageTitle = GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w800, color: MyColor.blue);
+  TextStyle header = GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w800, color: MyColor.black);
+
+  TextStyle bodyHeader = GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w800, color: MyColor.black);
+ }
